38c9c52d95c13af916e5d6858110a39012896f54
mrtalley <mrtalley@iastate.edu>
2018-09-03 17:08:57 -0500

    Added about page

---
+++ src/app/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <div className='container is-desktop'>
+      <div className='is-size-2 has-text-centered'>
+        About Iowa State IEEE
+      </div>
+      <div className='has-text-centered'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus  sodales purus,vitae dictum orci. Sed in pretium neque. Fusce malesuada augue in feugiat faucibus. Nullam dolor metus, elementum in dignissim  a5non, maximus vel leo.
+      </div>
+    </div>
+    <p>This is the about page</p>
+  </div>
+);
+
+export default About;
