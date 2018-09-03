7686b1209d4dec03d2579db1745bbde73528dcc5
mrtalley <mrtalley@iastate.edu>
2018-09-03 17:09:23 -0500

    Add Calendar page

---
+++ src/app/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const calendar = () => (
+  <iframe src="https://calendar.google.com/calendar/embed?src=2jj1ptuu2kqthq4mikg0qvam40%40group.calendar.google.com&ctz=America%2FChicago" style={{
+    borderTop: 0,
+    borderBottom: 0,
+    borderLeft: 0,
+    borderRight: 0
+  }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+);
+
+export default calendar;
