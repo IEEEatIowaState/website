a508b0c9c00557400364fcb571a97861be28a091
mrtalley <mrtalley@iastate.edu>
2018-09-05 18:14:43 -0500

    Add content for about page

---
+++ src/assets/json/executiveTeam.js
@@ -0,0 +1,109 @@
+import president from '../images/boardMembers/jared-griffin.jpg';
+// import vicePresident from '';
+import treasurer from '../images/boardMembers/ben-gisler.png';
+// import secretary from '';
+// import industryRelations from '';
+// import membership from '';
+// import eventsActivities from '';
+// import esc from '';
+// import ee from '';
+// import cpre from '';
+// import fundraising from '';
+import web from '../images/boardMembers/maxwell-talley.jpg';
+import project from '../images/boardMembers/dennis-xu.jpg';
+
+var members = [
+  {
+    "position": "President",
+    "name": "Jared Griffin",
+    "email": "ieeepres@iastate.edu",
+    "image": president,
+    "bio": "Jared is a senior in Computer Engineering from Newton, IA. His technical background consists of web and app development from his internships. Jared enjoys playing disc golf in his free time.",
+  },
+  {
+    "position": "Vice President",
+    "name": "Amir Hamza",
+    "email": "ieeepres@iastate.edu",
+    "image": 'vicePresident',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Treasurer",
+    "name": "Ben Gisler",
+    "email": "bigisler@iastate.edu",
+    "image": treasurer,
+    "bio": "I’m from Des Moines, Iowa, and very much enjoy digital logic, electornics, and computers. Great expectations of my senior year!",
+  },
+  {
+    "position": "Secretary",
+    "name": "Ethan Williams",
+    "email": "ethwillz@iastate.edu",
+    "image": 'secretary',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Industry Relations",
+    "name": "Alec Harrison",
+    "email": "alech@iastate.edu",
+    "image": 'industryRelations',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Membership",
+    "name": "Michael Scholl",
+    "email": "mlscholl@iastate.edu",
+    "image": 'membership',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Events and Activities",
+    "name": "Nicholas Chockalingam",
+    "email": "nickchoc@iastate.edu",
+    "image": 'eventsActivities',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "ESC Representative",
+    "name": "Isaiah Exley-Schuman",
+    "email": "isaiahe@iastate.edu",
+    "image": 'esc',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "EE Curriculum & Project Chair",
+    "name": "Nihaal Sitaraman",
+    "email": "nihaals@iastate.edu",
+    "image": 'ee',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "CprE Curriculum Chair",
+    "name": "Claudia Athens",
+    "email": "cathens@iastate.edu",
+    "image": 'cpre',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Fundraising Chair",
+    "name": "Robert Buckley",
+    "email": "rbuckley@iastate.edu",
+    "image": 'fundraising',
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Web Chair",
+    "name": "Maxwell Talley",
+    "email": "mrtalley@iastate.edu",
+    "image": web,
+    "bio": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris consectetur adipiscing elit.",
+  },
+  {
+    "position": "Project Chair",
+    "name": "Dennis Xu",
+    "email": "dgxu@iastate.edu",
+    "image": project,
+    "bio": "Dennis is a senior computer engineering from John, Iowa. He is doing research in the field regarding sensor networks. He was also an intern for Western digital.",
+  },
+];
+
+export default members;
