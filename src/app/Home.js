c695839248b288a24f2a23a9911684f8bf1ef51b
mrtalley <mrtalley@iastate.edu>
2018-09-20 14:40:38 -0500

    Edit home page content

--- src/app/Home.js
+++ src/app/Home.js
@@ -7,43 +7,48 @@ const Home = () => (
     <section className='hero is-large hero-image'>
       <div className='hero-body'>
         <div className='container has-text-centered is-size-4'>
-          Description about our club: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed imperdiet suscipit purus quis finibus. Ut et aliquam lorem, non pretium erat. Nulla mollis mauris ut sollicitudin dapibus. Vestibulum non lectus lobortis, venenatis libero vitae, aliquam libero.
+          Want to network with companies and get free pizza in the process?
         </div>
         <div className='has-text-centered'>
-          <a className='button is-link is-medium home-join-button'>
+          <a className='button is-link is-medium home-join-button' onClick={() => {
+            document.getElementById('header-modal')
+              .classList.add('is-active');
+          }}>
             Join the Club
           </a>
         </div>
       </div>
     </section>
     <div className='section full-height light-background'>
-      <div className='container columns'>
-        <div className='column is-one-third is-centered has-text-centered'>
-          <div>
-            <div className='is-size-5'>
-              Company talks, game nights, and Jeff's Pizza!
+      <div className='container is-desktop'>
+        <div className='columns'>
+          <div className='column is-one-third is-centered has-text-centered'>
+            <div>
+              <div className='is-size-5'>
+                Company talks, game nights, and Jeff's Pizza!
+              </div>
+              <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
+                What
+              </h4>
             </div>
-            <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
-              What
-            </h4>
           </div>
-        </div>
-        <div className='column is-one-third has-text-centered is-centered'>
-          <div>
-            <div className='is-size-5'>
-              Normally in Coover 3043, but check the calendar to be sure!
+          <div className='column is-one-third has-text-centered is-centered'>
+            <div>
+              <div className='is-size-5'>
+                Normally in Coover 3043, but check the calendar to be sure!
+              </div>
+              <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
+                Where
+              </h4>
             </div>
-            <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
-              Where
-            </h4>
           </div>
-        </div>
-        <div className='column is-one-third has-text-centered is-centered'>
-          <div>
-            <div className='is-size-5'>Wednesday Nights at 7pm</div>
-            <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
-              When
-            </h4>
+          <div className='column is-one-third has-text-centered is-centered'>
+            <div>
+              <div className='is-size-5'>Wednesday Nights at 7pm</div>
+              <h4 className='is-size-4 is-uppercase has-text-weight-bold'>
+                When
+              </h4>
+            </div>
           </div>
         </div>
       </div>
