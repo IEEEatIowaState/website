171223e49cf85d2282581e6b44ec8dd9021754fe
mrtalley <mrtalley@iastate.edu>
2018-10-01 20:38:07 -0500

    different cals for mobile and desktop

--- src/app/Calendar.js
+++ src/app/Calendar.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
-const calendar = () => (
-  <iframe src="https://calendar.google.com/calendar/embed?src=2jj1ptuu2kqthq4mikg0qvam40%40group.calendar.google.com&ctz=America%2FChicago" style={{
-    borderTop: 0,
-    borderBottom: 0,
-    borderLeft: 0,
-    borderRight: 0
-  }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
-);
+let width = window.innerWidth;
+let calendar = {};
+
+if(width > 768) {
+  calendar = () => (
+    <iframe src="https://calendar.google.com/calendar/embed?src=2jj1ptuu2kqthq4mikg0qvam40%40group.calendar.google.com&ctz=America%2FChicago"
+      style={{
+        borderTop: 0,
+        borderBottom: 0,
+        borderLeft: 0,
+        borderRight: 0,
+        width: '1200px',
+        height: '900px'
+      }}
+      frameborder="0"
+      scrolling="no"
+    >
+    </iframe>
+  );
+}
+
+else {
+  calendar = () => (
+    <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=2jj1ptuu2kqthq4mikg0qvam40%40group.calendar.google.com&amp;color=%23B1365F&amp;ctz=America%2FChicago"
+      style={{
+        borderWidth: 0,
+        width: '100%',
+        height: '600px'
+      }}
+      frameborder="0"
+      scrolling="no"
+    >
+    </iframe>
+  );
+}
 
 export default calendar;
